5e37c62b712ee76f7286de2a18948473906c5395
João Barboza <jprsbarboza@gmail.com>
2024-06-29 15:04:20 -0300

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,51 @@
+function comprar() {
+  let tipoIngresso = document.getElementById("tipo-ingresso").value;
+  let qtdIngresso = document.getElementById("qtd").value;
+  if (tipoIngresso == "pista") {
+    comprarPista(qtdIngresso);
+  } else if (tipoIngresso == "inferior") {
+    comprarInferior(qtdIngresso);
+  } else {
+    comprarSuperior(qtdIngresso);
+  }
+}
+
+function comprarPista(qtdIngresso) {
+  let qtdPistaTexto = document.getElementById("qtd-pista");
+  let qtdPistaValor = parseInt(qtdPistaTexto.innerHTML);
+  if (qtdIngresso > qtdPistaValor) {
+    alert(
+      `ERRO: Você digitou um valor maior que a quantidade de ingressos disponíveis para PISTA`
+    );
+  } else {
+    alert("Compra realizada com sucesso!");
+    qtdPistaValor = qtdPistaValor - qtdIngresso;
+    qtdPistaTexto.innerHTML = `${qtdPistaValor}`;
+  }
+}
+function comprarInferior(qtdIngresso) {
+  let qtdInferiorTexto = document.getElementById("qtd-inferior");
+  let qtdInferiorValor = parseInt(qtdInferiorTexto.innerHTML);
+  if (qtdIngresso > qtdInferiorValor) {
+    alert(
+      `ERRO: Você digitou um valor maior que a quantidade de ingressos disponíveis para Cadeira Inferior`
+    );
+  } else {
+    alert("Compra realizada com sucesso!");
+    qtdInferiorValor = qtdInferiorValor - qtdIngresso;
+    qtdInferiorTexto.innerHTML = `${qtdInferiorValor}`;
+  }
+}
+function comprarSuperior(qtdIngresso) {
+  let qtdSuperiorTexto = document.getElementById("qtd-superior");
+  let qtdSuperiorValor = parseInt(qtdSuperiorTexto.innerHTML);
+  if (qtdIngresso > qtdSuperiorValor) {
+    alert(
+      `ERRO: Você digitou um valor maior que a quantidade de ingressos disponíveis para Cadeira Superior`
+    );
+  } else {
+    alert("Compra realizada com sucesso!");
+    qtdSuperiorValor = qtdSuperiorValor - qtdIngresso;
+    qtdSuperiorTexto.innerHTML = `${qtdSuperiorValor}`;
+  }
+}
